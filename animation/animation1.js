7266caddc2dfcd0e8aec4afcd14de90f1f8d37a0
Anton Whalley <mrcowsy@gmail.com>
2012-03-24 00:10:16 +0000

    JS Session its spring lets get some stuff moving

---
+++ animation/animation1.js
@@ -0,0 +1,15 @@
+window.onload = setup;
+
+var logo;
+
+function setup() {
+  logo = document.getElementById('logo');// get the "image" object
+  logo.style.left = '0px'; // set its initial position to 0px
+  animate(); // start animating
+}
+
+function animate()
+{
+	logo.style.left = parseInt(logo.style.left) + 2 + 'px';
+	animator = setTimeout("animate()",20);
+}
---
+++ animation/animation2.js
@@ -0,0 +1,67 @@
+window.onload = setup;
+
+var winW = 630, winH = 460;
+var logo;
+var moveRight = true;
+var run = true;
+
+
+
+function setup() {
+  logo = document.getElementById('logo');; // get the "image" object
+  logo.style.left = '0px'; // set its initial position to 0px
+  logo.onclick=new Function("toggleAnimate()");
+  animate(); // start animating
+  	
+}
+
+function animate()
+{
+	if(parseInt(logo.style.left) + 50 > winW)
+	{
+		moveRight = false; 
+	}
+	if(parseInt(logo.style.left) < 1)
+	{
+		moveRight = true; 
+	}
+	
+	if(moveRight == true)
+	{
+		logo.style.left = parseInt(logo.style.left) + 2 + 'px';
+	}else
+	{
+		logo.style.left = parseInt(logo.style.left) - 2 + 'px';
+		
+	}
+	animator = setTimeout("animate()",20);
+}
+
+function toggleAnimate()
+{
+	run = !run;
+
+	if(run)
+	{
+		animator = setTimeout("animate()",20);
+	}else
+	{
+		clearTimeout(animator);
+	}
+}	
+
+
+if (document.body && document.body.offsetWidth) {
+ winW = document.body.offsetWidth;
+ winH = document.body.offsetHeight;
+}
+if (document.compatMode=='CSS1Compat' &&
+    document.documentElement &&
+    document.documentElement.offsetWidth ) {
+ winW = document.documentElement.offsetWidth;
+ winH = document.documentElement.offsetHeight;
+}
+if (window.innerWidth && window.innerHeight) {
+ winW = window.innerWidth;
+ winH = window.innerHeight;
+}
